bdf58e99ed3c63c2d45d6e5e3e20c6e765c3ec30
zheng <zhen.wang@target-energysolutions.com>
2018-09-23 21:12:02 +0800

    modify style for articleItem

--- src/component/article-item/index.js
+++ src/component/article-item/index.js
@@ -28,22 +28,27 @@ class ArticleItem extends PureComponent {
                         {id: 2, type: 'volumes', topinfo: {author: 'Duanbowen', time: '5分钟前', classify: '前端'}, title: '移动端页面分享快照生成总结', image: articleImg}]
     }
     state = {
+        clicked: false,
         likeActive: false,
         commentsActive: false
     }
-    onLikeClick = (index) => {
-        return function test() {
-            console.log(index)
-        };
-        // const oldLikeActive = t his.state.likeActive
-        // // console.log('id====', index)
-        // this.setState({
-        //     likeActive: oldLikeActive ? false : true
-        // })
+    handleArticleClick = () => {
+        this.setState({
+            clicked: true
+        })
+    }
+    onLikeClick = () => {
+        // return function test() {
+        //     console.log(index)
+        // };
+        const oldLikeActive = this.state.likeActive
+        this.setState({
+            likeActive: oldLikeActive ? false : true
+        })
     }
     render(){
         const { articleInfos } = this.props
-        const { likeActive, commentsActive } = this.state
+        const { clicked, likeActive, commentsActive } = this.state
         const renderMetaList = (metas) => {
             let metasList = []
             let id=0
@@ -63,9 +68,12 @@ class ArticleItem extends PureComponent {
                                 {renderMetaList(article.topinfo)}
                             </ul>
                         </div>
-                        <div className="title"><a>{article.title}</a></div>
+                        <div className={clicked ? 'articleitem-title clicked' : 'articleitem-title'}
+                            onClick={this.handleArticleClick}>
+                            <a>{article.title}</a>
+                        </div>
                          <div className="articleitem-buttons">
-                            <div className="articleitem-button1" onClick={this.onLikeClick(index)}>
+                            <div className="articleitem-button1" onClick={this.onLikeClick}>
                                 {likeActive ? (<img src={likeIconClick}/>) : (<img src={likeIcon}/>)}
                                 <span className={likeActive ? 'articleitem-buttons-count active': 'articleitem-buttons-count'}>{article.likeCounts}</span>
                             </div>
--- src/route/index.js
+++ src/route/index.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import ArticleItem from '../component/article-item';
 import Header from '../component/header';
 import Main from '../component/main';
 class WebAppRoute extends Component {
@@ -7,8 +6,6 @@ class WebAppRoute extends Component {
     return (
       <Fragment>
         {/* <Header /> */}
-        <ArticleItem />
-        <Header />
         <Main />
       </Fragment>
     );
