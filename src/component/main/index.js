019ab5e1c87c367cf26029faeb1790b1c88471db
zheng <zhen.wang@target-energysolutions.com>
2018-09-25 10:27:23 +0800

    finish main-header

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import avatar from '../../assets/avatar.png'
+import { navIcons } from '../../lib/const.js'
+import './style.less'
 class Main extends Component {
   render() {
     return (
-      <main>
-        main
+      <main className="main-container">
+        <nav className="maincontainer-nav">
+          <ul className="maincontainer-nav-left">
+            <div className="maincontainer-nav-avatar"><img src={avatar} className="maincontainer-nav-avatar"/></div>
+            <li className="maintainer-nav-item">
+              <img src={navIcons[0]}/>
+              <a>发沸点</a>
+            </li>
+            <li className="maintainer-nav-item">
+              <img src={navIcons[1]}/>
+              <a>写文章</a>
+            </li>
+            <li className="maintainer-nav-item">
+              <img src={navIcons[2]}/>
+              <a>分享链接</a>
+            </li>
+          </ul>
+          <div className="maincontainer-nav-draft">草稿</div>
+        </nav>
+        <div className="maincontainer-header">main header</div>
+        <div>articles</div>
       </main>
     );
   }
