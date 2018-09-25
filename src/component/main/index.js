cf4d151f24ff8cf1ad15adebb6777d5fc5ebb712
zheng <zhen.wang@target-energysolutions.com>
2018-09-25 13:57:42 +0800

    finish the main frame

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import avatar from '../../assets/avatar.png'
-import { navIcons } from '../../lib/const.js'
+import { navIcons, bookImgs } from '../../lib/const.js'
 import './style.less'
 class Main extends Component {
   render() {
@@ -24,8 +24,35 @@ class Main extends Component {
           </ul>
           <div className="maincontainer-nav-draft">草稿</div>
         </nav>
-        <div className="maincontainer-header">main header</div>
-        <div>articles</div>
+        <div className="maincontainer-entry-list">
+          <div className="maincontainer-list-header">
+            <nav>
+              <ul>
+                <li>热门</li>
+                <li>最新</li>
+                <li>评论</li>
+              </ul>
+              <ul>
+                <li>本周最热</li>
+                <li>本月最热</li>
+                <li>历史最热</li>
+              </ul>
+            </nav>
+          </div>
+          <ul>
+            <li>article items</li>
+          </ul>
+        </div>
+        <aside className="maincontainer-aside">
+          <div className="maincontainer-aside-item">
+            <div className="header">掘金小册</div>
+          </div>
+          <div className="maincontainer-aside-item">
+            <ul>
+              <li><a><img/><span>收藏集</span></a></li>
+            </ul>
+          </div>
+        </aside>
       </main>
     );
   }
