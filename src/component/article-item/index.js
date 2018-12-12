c29a7e4309323b77a1b5a81df78b3510f5734a42
zheng <zhen.wang@target-energysolutions.com>
2018-12-12 15:16:46 +0800

    add article item api

--- src/component/article-item/index.js
+++ src/component/article-item/index.js
@@ -5,30 +5,25 @@ import './style.less';
 
 class ArticleItem extends PureComponent {
     static propstypes = {
-        allArticles: PropTypes.arrayOf(
-            PropTypes.shape({
-                id: PropTypes.number,
-                title: PropTypes.string.isRequired,
-                image: PropTypes.string,
-                likeCounts: PropTypes.number,
-                commentCounts: PropTypes.number,
-            })
-        ).isRequired,
         onClick: PropTypes.func,
     }
-    static defaultProps = {
-        allArticles: [{
-            id: 1,
-            type: 'column',
-            topinfo: { author: 'WangZhen', time: '1小时前', classify: '前端\\JavaScript' },
-            title: '基于hapi的Node.js的程序后端开发',
-            likeCounts: 9,
-            commentCounts: 2,
-        },
-        { id: 2, type: 'volumes', topinfo: { author: 'Duanbowen', time: '5分钟前', classify: '前端' }, title: '移动端页面分享快照生成总结'}]
+    state = {
+        currentArticles: []
+    }
+    componentDidMount(){
+        fetch('http://rap2api.taobao.org/app/mock/117002/papers').then(function(response) {
+            return response.json()
+          }).then(artices => {
+               this.setState({
+                currentArticles: artices.data
+               })
+          })
     }
     handleContentScroll = e => {
         const { scrollTop, clientHeight, scrollHeight } = e.target
+        console.log('scrollTop------', scrollTop)
+        console.log('clientHeight------', clientHeight)
+        console.log('scrollHeight------', scrollHeight)
         const { allArticles } = this.props
         if (
             scrollHeight - (scrollTop + clientHeight) <= 200 &&
@@ -43,8 +38,7 @@ class ArticleItem extends PureComponent {
         this.list.scrollTop = 0
     }
     render() {
-        const allArticles = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-        const article = { id: 2, type: 'volumes', topinfo: { author: 'Duanbowen', time: '5分钟前', classify: '前端' }, title: '移动端页面分享快照生成总结'}
+        const { currentArticles } = this.state
         return (
             <div className="container">
                 <div 
@@ -52,7 +46,7 @@ class ArticleItem extends PureComponent {
                     ref={ref => (this.list = ref)}
                     onScroll={this.handleContentScroll}
                 >
-                    {allArticles.map((a, index) => <Article key={index} article={article}/>)}
+                    {currentArticles.map((article, index) => <Article key={index} article={article}/>)}
                 </div>
                 <button className="to-top-btn" onClick={this.handleToTopClick}>
                     <i className="ion-android-arrow-dropup"/>
